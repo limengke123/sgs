84b6b2967080892717fdb470a0cf2fdd2a737185
limengke <752140235@qq.com>
2020-05-21 10:32:16 +0800

    feat: option 配置页面支持 配置模版

--- src/js/options.ts
+++ src/js/options.ts
@@ -1,15 +1,29 @@
-import {defaultFunctionDeclarationTemplate} from "../template/defaultTemplate";
+import {
+    defaultFunctionDeclarationTemplate,
+    defaultRequestDeclarationTemplate,
+    defaultResponseDeclarationTemplate
+} from "../template/defaultTemplate";
 
 document.addEventListener('DOMContentLoaded', function() {
-    chrome.storage.sync.get({methodTemplate: defaultFunctionDeclarationTemplate}, (data) => {
+    chrome.storage.sync.get({
+        methodTemplate: defaultFunctionDeclarationTemplate,
+        requestTemplate: defaultRequestDeclarationTemplate,
+        responseTemplate: defaultResponseDeclarationTemplate
+    }, (data) => {
         document.querySelector<HTMLTextAreaElement>('#method').value = data.methodTemplate
+        document.querySelector<HTMLTextAreaElement>('#request').value = data.requestTemplate
+        document.querySelector<HTMLTextAreaElement>('#response').value = data.responseTemplate
     })
 });
 
 document.getElementById('save').addEventListener('click', function() {
     const methodTemplate = document.querySelector<HTMLTextAreaElement>('#method').value;
+    const requestTemplate = document.querySelector<HTMLTextAreaElement>('#request').value;
+    const responseTemplate = document.querySelector<HTMLTextAreaElement>('#response').value;
     chrome.storage.sync.set({
-        methodTemplate: methodTemplate
+        methodTemplate: methodTemplate,
+        requestTemplate: requestTemplate,
+        responseTemplate: responseTemplate
     }, () => {
         document.getElementById('status').textContent = '保存成功！'
         setTimeout(() => {document.getElementById('status').textContent = '';}, 800)
